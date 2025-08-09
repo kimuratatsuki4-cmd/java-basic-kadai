25c27d4d45d78b162e86ac06cfc1a6fb184198ca
kimuratatsuki4-cmd <kimuratatsuki4@gmail.com>
2025-08-10 00:20:36 +0900

    コードの整形とコメントの追加

--- java.study/src/kadai_015/Car_Chapter15.java
+++ java.study/src/kadai_015/Car_Chapter15.java
@@ -3,39 +3,40 @@ package kadai_015;
 public class Car_Chapter15 {
 	private int gear = 1;
 	private int speed = 10;
-	
+
+	/**
+	 * @param afterGear
+	 */
 	public void changeGear(int afterGear) {
-		// TODO 自動生成されたメソッド・スタブ
 		gear = afterGear;
 		switch (afterGear) {
-		case 1:
-			speed = 10;
-			break;
-		case 2:
-			speed = 20;
-			break;
-		case 3:
-			speed = 30;
-			break;
-		case 4:
-			speed = 40;
-			break;
-		case 5:
-			speed = 50;
-			break;
+			case 1:
+				speed = 10;
+				break;
+			case 2:
+				speed = 20;
+				break;
+			case 3:
+				speed = 30;
+				break;
+			case 4:
+				speed = 40;
+				break;
+			case 5:
+				speed = 50;
+				break;
 
-		default:
-			speed = 10;
-			break;
+			default:
+				speed = 10;
+				break;
 		}
-		System.out.println("ギア1から"+ afterGear + "に切り替えました");
-		
+		System.out.println("ギア1から" + afterGear + "に切り替えました");
 
 	}
+
 	public void run() {
-		System.out.println("速度は時速"+ speed + "kmです");
-		
+		System.out.println("速度は時速" + speed + "kmです");
+
 	}
-	
 
 }
